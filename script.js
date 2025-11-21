aac714cb45cf49ec48bb91ed1119859ae10fdd83
Cosmin Emil Bucur <cosminbucur2312@gmail.com>
2025-11-21 12:46:51 +0200

    made the frame function external

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.onkeydown = function (keyPressed) {
         console.log(position);
         if (position == 440) {
             position -= 150;
-        }   
+        }
         trex.style.top = position + "px";
         console.log(position, trex.style.top);
         setTimeout(() => {
@@ -16,42 +16,47 @@ document.onkeydown = function (keyPressed) {
     }
 }
 
+function frame(obstacle, pos, initialPos) {
+    if (pos == 10) {
+        pos = initialPos;
+    } else {
+        console.log(obstacle, pos, initialPos); 
+        if (checkCollision(obstacle, trex)) {
+            let message = document.getElementById("message");
+            message.innerText = "GAME OVER";
+            setTimeout(() => {
+                window.location.reload();
+            }, 2000);
+        }
+        --pos;
+        obstacle.style.left = pos + "px";
+    }
+}
+
 function generateGame() {
     let obstacles = document.getElementById("obstacles");
-    for(let i = 1; i <= 3; ++i) {
+    for (let i = 1; i <= 3; ++i) {
         let obstacle = document.createElement("div");
         obstacle.id = "obstacle" + i;
-        obstacles.appendChild(obstacle);   
+        obstacles.appendChild(obstacle);
         let pos = obstacle.getBoundingClientRect().left;
         initialPos = pos;
         let id = null;
         clearInterval(id);
-        id = setInterval(frame, 5);
-        function frame() {
-            if (pos == 10) { 
-                pos = initialPos;
-            } else {
-                if (checkCollision(obstacle, trex)) {
-                    let message = document.getElementById("message");
-                    message.innerText = "GAME OVER";
-                    setTimeout(() => {
-                        window.location.reload();
-                    }, 2000);
-                }
-                --pos;
-                obstacle.style.left = pos + "px";
-            }
-        }
-        console.log(pos);
+        setInterval(() => {
+            frame(obstacle, pos, initialPos);
+            --pos;
+        }, 5)
+        id = setInterval(frame, 10 * 1000, obstacle, pos - 1, initialPos);
     }
 }
 
 function checkCollision(obj1, obj2) {
     let obj1Pos = obj1.getBoundingClientRect();
     let obj2Pos = obj2.getBoundingClientRect();
-    if (!(obj1Pos.right < obj2Pos.left 
-        || obj1Pos.left > obj2Pos.right 
-        || obj1Pos.bottom < obj2Pos.top 
+    if (!(obj1Pos.right < obj2Pos.left
+        || obj1Pos.left > obj2Pos.right
+        || obj1Pos.bottom < obj2Pos.top
         || obj1Pos.top > obj2Pos.bottom)) {
         return 1;
     }
